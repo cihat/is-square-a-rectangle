3ce2ad0f71406e9ae111e444fe6050f8d371153b
cihat <cihat02020202@gmail.com>
2021-09-12 15:19:40 +0300

    Add answer that a square is not a rectangle

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ class Square extends Rectangle {
 }
 
 const rectangle = new Rectangle(10, 5)
+//* This works great
 const squre = new Square(10, 10)
 
 console.log(rectangle.getArea()) // 50
 console.log(squre.getArea()) // 100
+
+//! Here it explodes
+//! If we give the square two different side lengths as parameters. This code will not work here. So a square is not a rectangle.
+const squre = new Square(5, 10)
+console.log(squre.getArea()) // 50
